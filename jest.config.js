1c35e8c53af9f3ebf9915b9a48950dc595b41f4a
Sebastián Palacios <spalacios@spalaci-ltm7242.internal.salesforce.com>
2025-04-17 22:01:46 -0300

    a new testing pipeline configuration, hope it's the last one

--- jest.config.js
+++ jest.config.js
@@ -1,46 +1,55 @@
 /** @type {import('jest').Config} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.[tj]sx?$': 'ts-jest',
-  },
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
-  },
-  testMatch: [
-    '**/__tests__/**/*.test.[jt]s?(x)',
-  ],
-  roots: ['<rootDir>'],
-  maxWorkers: '50%',
-  detectOpenHandles: true,
-  forceExit: true,
-  collectCoverage: false,
-  collectCoverageFrom: [
-    'src/**/*.ts',
-    '!src/presentation/**',
-    '!**/node_modules/**',
-    '!**/*.d.ts'
-  ],
-  moduleFileExtensions: ['ts', 'js'],
-  coverageDirectory: '<rootDir>/coverage',
-  coverageReporters: ['text', 'lcov'],
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80
+  projects: [
+    {
+      displayName: 'domain',
+      preset: 'ts-jest',
+      testEnvironment: 'node',
+      testMatch: ['<rootDir>/__tests__/**/*.test.ts'],
+      transform: { '^.+\\.[tj]s?$': 'ts-jest' },
+      testPathIgnorePatterns: ['/node_modules/', '<rootDir>/src/presentation/'],
+      collectCoverageFrom: [
+        'src/**/*.ts',
+        '!src/presentation/**',
+        '!**/node_modules/**',
+        '!**/*.d.ts'
+      ],
+      coverageDirectory: '<rootDir>/coverage/domain',
+      coverageThreshold: {
+        global: {
+          branches: 80,
+          functions: 80,
+          lines: 80,
+          statements: 80,
+        }
+      }
+    },
+    {
+      displayName: 'presentation',
+      preset: 'ts-jest',
+      testEnvironment: 'jsdom',
+      rootDir: 'src/presentation',
+      testMatch: ['**/__tests__/**/*.test.ts?(x)'],
+      transform: { '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: './src/presentation/tsconfig.test.json' }] },
+      moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/../$1'
+      },
+      testPathIgnorePatterns: ['/node_modules/'],
+      collectCoverageFrom: [
+        '**/*.{ts,tsx}',
+        '!**/node_modules/**',
+        '!**/*.d.ts',
+        '!**/.next/**',
+      ],
+      coverageDirectory: '<rootDir>/../../coverage/presentation',
+      coverageThreshold: {
+        global: {
+          branches: 80,
+          functions: 80,
+          lines: 80,
+          statements: 80,
+        }
+      }
     }
-  },
-  watchPathIgnorePatterns: ['<rootDir>/node_modules/'],
-  testPathIgnorePatterns: [
-    '<rootDir>/node_modules/',
-    "<rootDir>/src/presentation/"
-  ],
-  watchPlugins: [
-    'jest-watch-typeahead/filename',
-    'jest-watch-typeahead/testname'
-  ],
-  
+  ]
 };
